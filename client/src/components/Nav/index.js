3054d52b0098d0172ef2f6d19fdabe688e39d788
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-27 19:45:57 -0700

    update navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function Nav() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">
-                React Reading List
-            </a>
-        </nav>
-    );
+function Navigation() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Google Books Search n' Save</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/search">Search</Nav.Link>
+          <Nav.Link href="/saved">Saved</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Nav;
+export default Navigation;
