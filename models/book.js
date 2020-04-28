f06335e1beee44275444140461fc8134820eb995
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-28 01:50:38 -0700

    update models

--- models/book.js
+++ models/book.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
     title: { type: String, required: true },
-    author: { type: String, required: true },
-    synopsis: String,
-    date: { type: Date, default: Date.now }
+    authors: { type: [String], required: true },
+    description: { type: String },
+    link: { type: String, required: true },
+    image: { type: String, required: true }
 });
 
 const Book = mongoose.model(`Book`, bookSchema);
